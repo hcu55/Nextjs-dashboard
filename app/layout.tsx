da77958c45297b009edda6a42eb603003944ebda
ChanUi <hcu55@naver.com>
2024-07-30 15:20:03 +0900

    Next js Dashboard practice

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+// #2-1-2. 폰트 불러오기
+import { inter } from './ui/fonts';
+export default function RootLayout({ children, }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* #2-1-2. body 태그에 inter 폰트 적용 */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+// 외부에서 import하여 사용할 수 있도록 작성
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
